2a3581474f509964d189d148dc14b9d55478090c
Frederico Bormann <github@frederico.info>
2020-11-09 20:42:34 +0100

    Add Question model

--- modumat_app/admin.py
+++ modumat_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from modumat_app.models import Module
+from modumat_app.models import Module, Question
 
 
 admin.site.register(Module)
+admin.site.register(Question)
--- modumat_app/models.py
+++ modumat_app/models.py
@@ -8,3 +8,11 @@ class Module(models.Model):
 
     def __str__(self):
         return f"{self.shorthand}: {self.module_name}"
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    full_question = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
