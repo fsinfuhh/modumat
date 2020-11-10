1f1e2f45290a2757050d2598e7e159c84ab40ec0
Frederico Bormann <github@frederico.info>
2020-11-10 10:47:09 +0100

    Add ModuleAnswer model

--- modumat_app/admin.py
+++ modumat_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from modumat_app.models import Module, Question
+from modumat_app.models import Module, Question, ModuleAnswer
 
 
 admin.site.register(Module)
 admin.site.register(Question)
+admin.site.register(ModuleAnswer)
--- modumat_app/models.py
+++ modumat_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+ANSWER_OPTIONS = (
+    (-1, 'negative'),
+    (0, 'neutral'),
+    (1, 'positive'),
+)
+
+
 class Module(models.Model):
     shorthand = models.CharField(max_length=20)
     module_name = models.CharField(max_length=50)
@@ -16,3 +23,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ModuleAnswer(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.IntegerField(choices=ANSWER_OPTIONS)
+
+    def __str__(self):
+        return f"{self.question.title} - {self.module.shorthand}: {ANSWER_OPTIONS[self.answer+1][1]}"
