b92e96d21d936366c21bfbe10043a45c617e27e9
Frederico Bormann <github@frederico.info>
2020-11-16 20:28:09 +0100

    Add design for question

--- modumat_app/views.py
+++ modumat_app/views.py
@@ -1,12 +1,14 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 
 def question(request, question_id: int):
-    try:
-        requested_question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'modumat_app/question.html', {'requested_question': requested_question})
+    requested_question = get_object_or_404(Question, pk=question_id)
+    all_questions = Question.objects.all()
+    return render(request, 'modumat_app/question.html',
+                  {
+                      'requested_question': requested_question,
+                      'all_questions': all_questions
+                  })
